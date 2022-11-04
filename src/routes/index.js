3bcb907149e7a46bbbd025a456f637bb9bfd2153
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2022-11-04 19:51:48 -0300

    Config rootImport
    Configurando e utilizando rootImport

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
+
+import SingIn from '~/pages/SignIn';
+import SingUp from '~/pages/SingUp';
+
+import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 import Route from './Route';
-
-import SingIn from '../pages/SignIn';
-import SingUp from '../pages/SingUp';
-
-import Dashboard from '../pages/Dashboard';
-import Profile from '../pages/Profile';
 
 export default function Routes() {
   return (
