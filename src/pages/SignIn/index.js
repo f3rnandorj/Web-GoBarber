75a4d69fe0feb6a3e651ca5e5577c0297436b9a0
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2022-11-04 21:51:30 -0300

    Auth style
    Configurada a parte de estilos de autenticação.

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
-function SignIn() {
-  return <h1>SignIn</h1>;
+export default function SignIn() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Digite sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Crie sua conta</Link>
+      </form>
+    </>
+  );
 }
-
-export default SignIn;
--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
-function SingUp() {
-  return <h1>Login</h1>;
+export default function SignIn() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Digite sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho conta</Link>
+      </form>
+    </>
+  );
 }
-
-export default SingUp;
