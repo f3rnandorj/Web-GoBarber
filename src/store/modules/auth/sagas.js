d4004c959f71004e8e18951da7723af699e616a2
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2022-11-07 17:43:25 -0300

    Sign failure config
    Primeira configuração de LogOut da aplicação

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -3,24 +3,28 @@ import { takeLatest, call, put, all } from 'redux-saga/effects';
 import history from '~/services/history';
 import api from '~/services/api';
 
-import { signInSuccess } from './actions';
+import { signFailure, signInSuccess } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  // primeiro parametro URL, segundo parametro e o que queremos enviar
-  const response = yield call(api.post, 'sessions', { email, password });
+    // primeiro parametro URL, segundo parametro e o que queremos enviar
+    const response = yield call(api.post, 'sessions', { email, password });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('Usuário não e prestador de serviços');
-    return;
+    if (!user.provider) {
+      console.tron.error('Usuário não e prestador de serviços');
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
