9b458b7b4ec7dab38a3b4807a71a01cbf259155d
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2022-11-10 22:12:08 -0300

    Config Profile update
    Configurando alterações no perfil do usuario.

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
-function Profile() {
-  return <h1>Profile</h1>;
+import { Container } from './styles';
+
+export default function Profile() {
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input
+          name="password"
+          type="oldPassword"
+          placeholder="Sua senha antiga"
+        />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="password"
+          type="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
-
-export default Profile;
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -21,7 +21,6 @@ export function* signIn({ payload }) {
     }
 
     api.defaults.headers.Authorization = `Bearer ${token}`;
-
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard');
